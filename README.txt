df381622061b413d551b8a976d44cb63169f4fc5
Soybeanman504 <70052390+Soybeanman504@users.noreply.github.com>
2022-01-31 18:25:14 +0900

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,81 @@
+[ patapata Ver1.00 - README.txt ]
+
+*** このプログラムについて ***
+・このプログラムはウェブページ上で「反転フラップ式案内表示機(以下、ぱたぱたとする)」風の表示をできるようにするものです。
+・製作者はSoybeanman(TwitterID : @Soybeanman504)です。
+---
+
+*** 利用条件 ***
+・ライセンスはGNU General Public License (GPL)です。
+    ・大体の内容を以下に示します。
+    作成者や著作権者は、製品に対しての義務や責任を何ら負わないこと
+    著作権の表示を保持すること
+    改変、再頒布の前提としてソースコードへのアクセスができること
+    製品を改変した場合は、その変更点を示すこと
+    製品を改変した場合もGPLで配布すること
+
+・利用条件について不明な点がある場合には、製作者(TwitterID : @Soybeanman504)へのダイレクトメッセージで受け付けます。
+・もし、他の方に紹介していただける場合にはGithubのURL( https://github.com/Soybeanman504/patapata )も一緒に紹介していただけるとありがたいです。
+---
+
+*** ファイルとディレクトリ一覧 ***
+・patapata.js
+・patapata.css
+    上記の2ファイルは実際に使う際に必要なものです。
+
+・README.txt
+    このテキストファイルです。不明なことがあればまずこれを参照してください。
+・sample
+    ・index.html
+    ・index.js
+    ・index.css
+        実際に使用した例です。
+        sampleは使用する上で不要なので消してもらって構いません。
+---
+
+*** HTML内への設置方法 ***
+[1] patapata.jsとpatapata.cssをわかりやすい場所に置きます。
+[2] 使用するページのHTMLの<head>内からpatapata.jsとpatapata.cssを読み込みます。
+[3] ぱたぱたを設置したい場所に以下のソースを置きます。
+        <div class="patapata"></div>
+    ・data-text属性で表示するテキストを指定できます。
+    ・大きさを変更する際はstyle属性のwidth,heightで指定できます。
+    ・フォントサイズの変更はできません。
+    ・デフォルトではテキスト無しで、大きさは120px*50px(縦*横)です。
+[4] 完了です。
+    ・この状態ではアニメーションは発生しません。
+        なんらかのJSファイルからPatapataクラスにある関数を使用して設定してください。
+        関数の詳細は次項「収録されているクラスと関数」で解説します。
+---
+
+*** 収録されているクラスと関数 ***
+・全てpatapata.jsに収録されています。
+
+[クラス一覧]
+・Patapataクラス
+    ・これを使用してアニメーションを指定します。
+    ・収録されている唯一のクラスです。
+
+[関数]
+・Patapata.change(parent, text, milliTime, frequency)
+    ・アニメーションして表示テキストを変更します。
+    ・parent : 対象とするぱたぱたの<div class="patapata">要素オブジェクト
+    ・text : 変更後のテキスト
+        ・初期値 ' '
+    ・milliTime : アニメーション時間
+        ・ミリ秒で指定します。
+        ・初期値 500
+    ・frequency : ぱたぱたする回数
+        ・初期値 5
+    ・呼び出し直後にアニメーションします。
+
+・Patapata.set(parent)
+    ・指定された<div class="patapata">要素に対してDOM操作を行います。
+    ・parent : 対象とするぱたぱたの<div class="patapata">要素オブジェクト
+    ・動的に<div class="patapata">要素を追加した場合にはこの関数を実行するとPatapata.changeを使用できるようになります。
+
+・Patapata.setAll()
+    ・内容が空の<div class="patapata">要素全てに対してDOM操作を行います。
+    ・これを実行することでPatapata.changeを使用できるようになります。
+    ・patapata.js上でDOMツリーの構築終了後に呼び出されているのでわざわざ使用する必要はないです。
+---
---
+++ patapata.js
@@ -0,0 +1,150 @@
+/*
+    This .js file is made by Soybeanman(TwitterID : @Soybeanman504).
+    The original source exists in https://github.com/Soybeanman504/patapata
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    Patapata.setAll();
+
+});
+
+class Patapata {
+
+    static setAll() {
+
+        var contents = document.getElementsByClassName('patapata');
+
+        for (var n = 0; n < contents.length; n++) {
+
+            Patapata.set(contents[n]);
+
+        }
+
+    }
+
+    static set(parent) {
+
+        if (parent.firstElementChild !== null) {
+            return;
+        }
+
+        var text = parent.dataset.text;
+
+        var childrensName = ['main', 'top', 'top', 'bottom', 'bottom'];
+
+        for (var n = 0; n < childrensName.length; n++) {
+
+            var child = document.createElement('div');
+            var divText = document.createElement('div');
+            var pText = document.createElement('p');
+
+            pText.innerText = text;
+            pText.style.fontSize = (parent.clientHeight * 0.8) + 'px';
+            pText.style.margin = (parent.clientHeight * 0.1) + 'px 0'
+
+            divText.className = 'patapata-text';
+            divText.appendChild(pText);
+
+            child.className = 'patapata-' + childrensName[n];
+            child.appendChild(divText);
+            parent.appendChild(child);
+
+        }
+
+        console.log(parent.clientHeight)
+
+    }
+
+    static change(parent, text = ' ', milliTime = 500, frequency = 5) {
+
+        var originalText = parent.dataset.text;
+
+        if (originalText === '') {
+            return false;
+        }
+
+        if (text === '') {
+            text = ' ';
+        }
+
+        var time = milliTime / 1000;
+        var mainText = parent.getElementsByClassName('patapata-main')[0];
+        var topText = parent.getElementsByClassName('patapata-top');
+        var bottomText = parent.getElementsByClassName('patapata-bottom');
+
+        if (text === null) {
+            text = originalText;
+        }
+
+        parent.dataset.text = '';
+
+        if (frequency > 1) {
+
+            Patapata.setText(mainText, text);
+            Patapata.setText(topText[0], originalText);
+            Patapata.setText(topText[1], '');
+            Patapata.setText(bottomText[0], '');
+            Patapata.setText(bottomText[1], originalText);
+
+            setTimeout(() => {
+                Patapata.setText(topText[0], '');
+                Patapata.setText(bottomText[1], '');
+            }, (milliTime / frequency) / 2);
+
+            setTimeout(() => {
+                Patapata.setText(topText[1], text);
+                Patapata.setText(bottomText[0], text);
+            }, milliTime - (milliTime / frequency));
+
+        } else {
+
+            Patapata.setText(mainText, text);
+            Patapata.setText(topText[0], originalText);
+            Patapata.setText(topText[1], text);
+            Patapata.setText(bottomText[0], text);
+            Patapata.setText(bottomText[1], originalText);
+
+        }
+
+        topText[0].style.zIndex = '30';
+        topText[0].style.animationName = 'patapata-smaller';
+        topText[0].style.animationDuration = (time / frequency) + 's';
+        topText[0].style.animationIterationCount = frequency;
+
+        topText[1].style.zIndex = '20';
+
+        bottomText[0].style.zIndex = '30';
+        bottomText[0].style.animationName = 'patapata-bigger';
+        bottomText[0].style.animationDuration = (time / frequency) + 's';
+        bottomText[0].style.animationIterationCount = frequency;
+
+        bottomText[1].style.zIndex = '20';
+
+        parent.addEventListener('animationend', () => {
+
+            topText[0].style.zIndex = '0';
+            topText[0].style.animationName = '';
+            topText[0].style.transform = '';
+
+            topText[1].style.zIndex = '0';
+
+            bottomText[0].style.zIndex = '0';
+            bottomText[0].style.animationName = '';
+            bottomText[0].style.transform = '';
+
+            bottomText[1].style.zIndex = '0';
+
+            parent.dataset.text = text;
+
+        });
+
+        return true;
+
+    }
+
+    static setText(element, text) {
+        element.firstElementChild.firstElementChild.innerText = text;
+    }
+
+}
